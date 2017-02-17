a60633f624da19420b493221d938cf35416c08d2
easter <easter.by@gmail.com>
2017-02-17 18:08:23 +0300

    разрешает curl переадресацию

--- inc/classes/Tool.php
+++ inc/classes/Tool.php
@@ -501,6 +501,7 @@ class Tool {
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_CONNECTTIMEOUT_MS, 7000);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
         curl_setopt($curl, CURLOPT_HEADER, $headers);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 		if ($data) {
