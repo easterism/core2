2049bc2bf14897909d60bfccfa5d761d2ce09a00
shabuninil <shabuninil24@gmail.com>
2017-07-28 13:03:15 +0300

    В панелях добавлены позиции и виды табов

--- inc/classes/Panel.php
+++ inc/classes/Panel.php
@@ -8,6 +8,14 @@ require_once 'Templater3.php';
  */
 class Panel {
 
+    const POSITION_TOP    = 1;
+    const POSITION_LEFT   = 2;
+    const POSITION_RIGHT  = 3;
+    const POSITION_BOTTOM = 4;
+
+    const TYPE_TABS  = 10;
+    const TYPE_PILLS = 20;
+
     protected $active_tab     = '';
     protected $title          = '';
     protected $content        = '';
@@ -15,6 +23,8 @@ class Panel {
     protected $tabs           = array();
     protected $theme_src      = '';
     protected $theme_location = '';
+    protected $position       = self::POSITION_TOP;
+    protected $type           = self::TYPE_TABS;
 
 
     /**
@@ -25,6 +35,44 @@ class Panel {
         $this->resource = $resource;
         if (isset($_GET[$this->resource])) {
             $this->active_tab = $_GET[$this->resource];
+        }
+    }
+
+
+    /**
+     * Установка позиции
+     * @param  int $position
+     * @throws Exception
+     */
+    public function setPosition($position) {
+        $positions = array(
+            self::POSITION_TOP,
+            self::POSITION_LEFT,
+            self::POSITION_RIGHT,
+            self::POSITION_BOTTOM
+        );
+        if (in_array($position, $positions)) {
+            $this->position = $position;
+        } else {
+            throw new Exception('Invalid position');
+        }
+    }
+
+
+    /**
+     * Установка типа для закладок
+     * @param  int $type
+     * @throws Exception
+     */
+    public function setTypeTabs($type) {
+        $types = array(
+            self::TYPE_TABS,
+            self::TYPE_PILLS
+        );
+        if (in_array($type, $types)) {
+            $this->type = $type;
+        } else {
+            throw new Exception('Invalid type');
         }
     }
 
@@ -82,13 +130,20 @@ class Panel {
     /**
      * Создание и возврат контейнера
      * @return string
+     * @throws Exception
      */
     public function render() {
 
         $tpl = new Templater3(DOC_ROOT . "/core2/html/" . THEME . '/html/panel.html');
 
         $tpl->assign('[RESOURCE]', $this->resource);
-        $tpl->assign('[CONTENT]',  $this->content);
+
+        if ($this->position == self::POSITION_BOTTOM) {
+            $tpl->content_top->assign('[CONTENT]', $this->content);
+        } else {
+            $tpl->content_bottom->assign('[CONTENT]', $this->content);
+        }
+
 
         if ( ! empty($this->title)) {
             $tpl->title->assign('[TITLE]', $this->title);
@@ -101,6 +156,22 @@ class Panel {
         }
 
         if ( ! empty($this->tabs)) {
+            switch ($this->type) {
+                case self::TYPE_TABS :  $type_name = 'tabs'; break;
+                case self::TYPE_PILLS : $type_name = 'pills'; break;
+                default : throw new Exception('Invalid type'); break;
+            }
+            $tpl->tabs->assign('[TYPE]', $type_name);
+
+            switch ($this->position) {
+                case self::POSITION_TOP :    $position_name = 'top'; break;
+                case self::POSITION_LEFT :   $position_name = 'left'; break;
+                case self::POSITION_RIGHT :  $position_name = 'right'; break;
+                case self::POSITION_BOTTOM : $position_name = 'bottom'; break;
+                default : throw new Exception('Invalid position'); break;
+            }
+            $tpl->tabs->assign('[POSITION]', $position_name);
+
             foreach ($this->tabs as $tab) {
 
                 if ($tab['disabled']) {
