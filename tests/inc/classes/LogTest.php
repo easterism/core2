b7e22fcc8b791b7f3e9ad2da94a39a1303c99108
GrinkevichVM <GrinkevichVM@belhard.com>
2018-04-13 11:05:01 +0300

    Merge branch 'master' of https://github.com/shabuninil/core2
    # Conflicts:
    #	html/material/js/class.list.js

--- tests/inc/classes/LogTest.php
+++ tests/inc/classes/LogTest.php
@@ -11,7 +11,46 @@ namespace Tests;
 use Core2\Log;
 use PHPUnit\Framework\TestCase;
 
+require_once DOC_ROOT . 'core2/inc/classes/Log.php';
+
 class LogTest extends TestCase
 {
 
+    /**
+     *
+     */
+    public function test__construct()
+    {
+        $this->assertTrue(true,true);
+    }
+
+    public function test__call()
+    {
+        $this->assertTrue(true,true);
+    }
+
+    public function testFile()
+    {
+        $this->assertTrue(true,true);
+    }
+
+    public function testAccess()
+    {
+        $this->assertTrue(true,true);
+    }
+
+    public function testInfo()
+    {
+        $this->assertTrue(true,true);
+    }
+
+    public function testWarning()
+    {
+        $this->assertTrue(true,true);
+    }
+
+    public function testDebug()
+    {
+        $this->assertTrue(true,true);
+    }
 }
