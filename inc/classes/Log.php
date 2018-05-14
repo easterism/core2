42d2f5722de2c1628025e9e51d8812ca719567cc
Victor <SachkovskiyVV@belhard.com>
2018-05-14 11:42:09 +0300

    Дополнение описания методов в Log

--- inc/classes/Log.php
+++ inc/classes/Log.php
@@ -58,7 +58,11 @@ class Log {
             $this->log = new Logger($name);
         }
     }
-
+    /**
+     * Обработчик метода не доступного через экземпляр
+     * @param string    $name       Имя метода
+     * @param array     $arguments  Параметры метода
+     */
     public function __call($name, $arguments)
     {
         if ($name == 'slack') {
@@ -194,6 +198,10 @@ class Log {
         }
     }
 
+    /**
+     * Установка обработчика
+     * @param int $level уровень журналирования
+     */
     private function setHandler($level) {
         while ($this->log->getHandlers()) {
             $this->log->popHandler();
