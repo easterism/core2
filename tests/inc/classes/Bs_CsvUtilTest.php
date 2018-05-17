b08275fc49911c4a57f4ccf539ed803a558637a3
GrinkevichVM <GrinkevichVM@belhard.com>
2018-05-17 15:48:27 +0300

    fix previs commit

--- tests/inc/classes/Bs_CsvUtilTest.php
+++ tests/inc/classes/Bs_CsvUtilTest.php
@@ -2,16 +2,52 @@
 /**
  * Created by PhpStorm.
  * User: GrinkevichVM
- * Date: 17.05.2018
- * Time: 14:30
+ * Date: 03.04.2018
+ * Time: 11:43
  */
 
-namespace Core2;
+namespace Tests;
 
-use Bs_CsvUtil;
+use Core2\Log;
 use PHPUnit\Framework\TestCase;
+
+require_once DOC_ROOT . 'core2/inc/classes/Log.php';
 
 class Bs_CsvUtilTest extends TestCase
 {
 
+    public function testBs_CsvUtil()
+    {
+        $this->assertTrue(true,true);
+    }
+
+    public function testCsvStringToArray()
+    {
+        $this->assertTrue(true,true);
+    }
+
+    public function testCsvArrayToArray()
+    {
+        $this->assertTrue(true,true);
+    }
+
+    public function testArrayToCsvString()
+    {
+        $this->assertTrue(true,true);
+    }
+
+    public function test_checkMultiline()
+    {
+        $this->assertTrue(true,true);
+    }
+
+    public function testCsvFileToArray()
+    {
+        $this->assertTrue(true,true);
+    }
+
+    public function test_valToCsvHelper()
+    {
+        $this->assertTrue(true,true);
+    }
 }
