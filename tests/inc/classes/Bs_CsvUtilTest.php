24fc90de5ded95bebe87c9029511bda2cf76e837
GrinkevichVM <GrinkevichVM@belhard.com>
2018-05-17 14:35:40 +0300

    template test for class Bs_CsvUtil

---
+++ tests/inc/classes/Bs_CsvUtilTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: GrinkevichVM
+ * Date: 17.05.2018
+ * Time: 14:30
+ */
+
+namespace Core2;
+
+use Bs_CsvUtil;
+use PHPUnit\Framework\TestCase;
+
+class Bs_CsvUtilTest extends TestCase
+{
+
+}
