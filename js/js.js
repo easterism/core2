1ce06ddd526279cd50ec986b98042518adc345ac
nevstas <nevstas@gmail.com>
2018-07-13 16:05:44 +0300

    Верхняя панелька с кнопкой закрыть при печати PDF в шкурке default

--- js/js.js
+++ js/js.js
@@ -247,6 +247,9 @@ var load = function (url, data, id, callback) {
 	}
 	var h = preloader.prepare(location.hash.substr(1));
 	url = preloader.prepare(url);
+
+    $("body").css("overflow", "auto");
+
 	if (!data && h != url && url.indexOf('&__') < 0) {
 		document.location.hash = url;
 	} else {
@@ -349,18 +352,35 @@ var load = function (url, data, id, callback) {
 
 var loadPDF = function (url) {
 	preloader.show();
-    $("#main_body").height($("body").height() - ($("#menuContainer").height() + 15));
-	$("#main_body").html('<iframe id="pdfiframe" frameborder="0" width="100%" height="100%"></iframe>');
-	$("#pdfiframe").attr('src', url);
+    $("#main_body").prepend(
+        '<div class="pdf-tool-panel hide"><button class="btn buttonSmall" onclick="removePDF();">Закрыть</button></div>' +
+        '<div class="pdf-main-panel hide"><iframe id="pdfiframe" frameborder="0" width="100%" height="100%" src="' + url + '"></iframe></div>'
+    );
+
+    $("#main_body > .pdf-main-panel").css({
+        'height'      : ($("body").height() - ($("#menuContainer").height()) - 41),
+        'margin-top'  : (($("#menuContainer").height()) + ($(".pdf-tool-panel").height()) - 55),
+    });
+
+    $("body").css("overflow", "hidden");
+
 	$("#pdfiframe").load( function() {
 		preloader.hide();
+        $('.pdf-tool-panel, .pdf-main-panel').removeClass('hide');
+        $(window).hashchange( function() {
+            $("body").css("overflow", "auto");
+        });
 	});
-
+}
+function removePDF() {
+    $('.pdf-tool-panel, .pdf-main-panel').remove();
+    $('body').css('overflow', 'auto');
 }
 
 function resize() {
-	$("#mainContainer").css('padding-top', $("#menuContainer").height() + 5);
+    $("#mainContainer").css('padding-top', $("#menuContainer").height() + 5);
     $("#main_body").height($("#rootContainer").height() - ($("#menuContainer").height() + 15));
+    $("#main_body > .pdf-main-panel").height(($("body").height() - ($("#menuContainer").height()) - 45));
 }
 
 $(function(){
