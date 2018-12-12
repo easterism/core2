c206e289ebdaf91d4fe51cbe3b89aac16c66c1fc
shabuninil <shabuninil24@gmail.com>
2018-12-12 18:05:55 +0300

    Исправление некорректного отображения формы входа

--- inc/classes/Init.php
+++ inc/classes/Init.php
@@ -421,9 +421,16 @@
             }
             else {
                 // GET LOGIN PAGE
-                if (!empty($_POST['xjxr']) && array_key_exists('X-Requested-With', Tool::getRequestHeaders())) {
-                    throw new Exception('expired');
+                if (array_key_exists('X-Requested-With', Tool::getRequestHeaders())) {
+                    if ( ! empty($_POST['xjxr'])) {
+                        throw new Exception('expired');
+                    }
+                    if ( ! empty($_GET['module'])) {
+                        http_response_code(403);
+                        return '';
+                    }
                 }
+
                 return $this->getLogin();
             }
 
