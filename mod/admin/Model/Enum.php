beb7f0f974ff92e0ef218a8a32efe141cfaa03f2
shabuninil <shabuninil24@gmail.com>
2022-07-11 11:33:30 +0300

    Исправление метода добавления в справочник

--- mod/admin/Model/Enum.php
+++ mod/admin/Model/Enum.php
@@ -10,39 +10,64 @@
 class Enum extends Zend_Db_Table_Abstract {
 
 	protected $_name = 'core_enum';
-	private $_enum = array();
+	private $_enum   = [];
 
-    public function createItem (string $global_id, string $value, array $custom = [],array $options = []) : int {
 
-        $customStr = '';
-        $last_key = array_key_last($custom);
+    /**
+     * Добавление записи в справочник
+     * @param string $global_id
+     * @param string $value
+     * @param array  $custom
+     * @param array  $options
+     * @return int
+     * @throws Exception
+     */
+    public function createItem (string $global_id, string $value, array $custom = [], array $options = []) : int {
+
+        $select = $this->select()
+            ->where('global_id = ?', $global_id);
+        $enum = $this->fetchRow($select);
+
+        if (empty($enum)) {
+            throw new \Exception(sprintf('Справочник %s не найден', $global_id));
+        }
+
+        $custom_fields = [];
 
         foreach ($custom as $key => $item) {
-            $key == $last_key ? $customStr .= $key.'::'.$item : $customStr .= $key.'::'.$item.':::';
+            if (is_scalar($item)) {
+                $custom_fields[] = "{$key}::{$item}";
+            }
         }
-        $parentId = $this->select()->from($this->_name, 'id');
-        $parentId->where('global_id = ?', $global_id);
-        $parentId = $this->fetchRow($parentId)->id;
 
-        $seq  = 1 + (int)$this->_db->fetchOne("
-            SELECT MAX(seq)
-            FROM core_enum AS ce
-            WHERE ce.parent_id = ?
-        ", $parentId);
+
+        $select = $this->select()
+            ->from($this->_name, ['max_seq' => new \Zend_Db_Expr('MAX(seq)')])
+            ->where('parent_id = ?', $enum->id);
+
+        $items = $this->fetchRow($select);
+        $seq   = 1 + (int)($items ? $items->max_seq : 0);
 
         $data = $this->createRow([
-            'parent_id' => $parentId,
-            'name' => $value,
+            'parent_id'     => $enum->id,
+            'name'          => $value,
             'is_default_sw' => $options['is_default'] ?? 'N',
-            'is_active_sw' => $options['is_active_sw'] ?? 'Y',
-            'seq' => $seq,
-            'custom_field' => $customStr
+            'is_active_sw'  => $options['is_active_sw'] ?? 'Y',
+            'lastuser'      => $options['lastuser'] ?? null,
+            'seq'           => $seq,
+            'custom_field'  => $custom_fields ? implode(':::', $custom_fields) : null
         ]);
-        return $data->save();
+
+        return (int)$data->save();
     }
 
-	public function exists($expr, $var = array())
-	{
+
+    /**
+     * @param $expr
+     * @param $var
+     * @return Zend_Db_Table_Row_Abstract|null
+     */
+	public function exists($expr, $var = []) {
 		$sel = $this->select()->where($expr, $var);
 		return $this->fetchRow($sel->limit(1));
 	}
