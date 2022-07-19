6c90d59b976ece885308d4685b84f5a94b19a731
easter <easter.by@gmail.com>
2022-07-19 19:31:08 +0300

    remove billing

--- mod/billing/v1.4.1/install/uninstall.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS `mod_billing_operations`;
-DELETE FROM core_settings WHERE `code` = 'billing_date_disable';
-DELETE FROM core_settings WHERE `code` = 'billing_mail_success_coming';
-DELETE FROM core_settings WHERE `code` = 'billing_mail_from';
-
-DELETE FROM mod_cron_jobs WHERE `module_id` = 'billing' AND module_method = 'cronReminderPay';
-DELETE FROM mod_cron_jobs WHERE `module_id` = 'billing' AND module_method = 'cronCheckOperations';
-DELETE FROM mod_cron_jobs WHERE `module_id` = 'billing' AND module_method = 'cronLicenseRenewals';
