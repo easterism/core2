87ecd925f975903850117194dbeea35533ee0413
shabuninil <shabuninil24@gmail.com>
2022-08-21 16:13:55 +0300

    Исправление ошибок связанных с календарями

--- html/material/js/coreui.table.js
+++ html/material/js/coreui.table.js
@@ -633,7 +633,7 @@ $(document).ready(function(){
             return false;
 
         } else {
-            $('.datepicker-container, .datetimepicker-container').hide('fast');
+            $('.coreui-table-wrapper .datepicker-container, .coreui-table-wrapper .datetimepicker-container').hide('fast');
         }
     });
 
