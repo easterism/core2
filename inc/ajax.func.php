f72d2dcb178bc1280466163bd5a6f4c5b520b393
easterism <easter.by@gmail.com>
2023-11-20 12:56:08 +0300

    контроль не строковых значений при сохранении

--- inc/ajax.func.php
+++ inc/ajax.func.php
@@ -87,7 +87,8 @@ class ajaxFunc extends Common {
 
 		foreach ($control as $field => $val) {
 			if (!is_array($val)) {
-				$control[$field] = $val ? trim($val) : "";
+				$control[$field] = is_string($val) ? trim($val) : "";
+
 				if (isset($control[$field . "%re"]) && $val !== $control[$field . "%re"]) {
 					$script .= "document.getElementById('" . $order_fields['mainTableId'] . $field . "2').className='reqField';";
 					$this->error[] = "- {$this->translate->tr('Пароль не совпадает.')}<br/>";
