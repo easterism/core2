a5a06b930977f010671d31697a55f329f1c059b1
shabuninil <shabuninil24@gmail.com>
2024-02-15 15:45:36 +0300

    Исправление edit dataset

--- inc/classes/class.edit.php
+++ inc/classes/class.edit.php
@@ -1554,12 +1554,10 @@ class editTable extends initEdit {
                                         foreach ($value['in'] as $item_field) {
                                             $field_value = '';
 
-                                            if ( ! empty($dataset)) {
-                                                if (isset($dataset[$item_field['code']])) {
-                                                    $field_value = is_string($dataset[$item_field['code']])
-                                                        ? $dataset[$item_field['code']]
-                                                        : '';
-                                                }
+                                            if ( ! empty($dataset) && isset($dataset[$item_field['code']])) {
+                                                $field_value = is_string($dataset[$item_field['code']]) || is_numeric($dataset[$item_field['code']])
+                                                    ? $dataset[$item_field['code']]
+                                                    : '';
                                             }
 
                                             $type_name = $item_field['type'] ?? 'text';
@@ -1570,9 +1568,6 @@ class editTable extends initEdit {
 
                                             if ($type_name == 'select') {
                                                 $field_value = $item_field['options'][$field_value] ?? $field_value;
-
-                                            } elseif ($type_name == 'textarea') {
-                                                $type_name = 'textarea';
 
                                             } elseif ($type_name == 'select2') {
                                                 $field_value = $item_field['options'][$field_value] ?? $field_value;
@@ -1639,13 +1634,10 @@ class editTable extends initEdit {
                                         foreach ($value['in'] as $item_field) {
                                             $field_value = '';
 
-                                            if ( ! empty($dataset)) {
-                                                if (isset($dataset[$item_field['code']])) {
-                                                    $field_value = is_string($dataset[$item_field['code']])
-                                                        ? $dataset[$item_field['code']]
-                                                        : '';
-
-                                                }
+                                            if ( ! empty($dataset) && isset($dataset[$item_field['code']])) {
+                                                $field_value = is_string($dataset[$item_field['code']]) || is_numeric($dataset[$item_field['code']])
+                                                    ? $dataset[$item_field['code']]
+                                                    : '';
                                             }
 
                                             $field_attributes = ! empty($item_field['attributes'])
