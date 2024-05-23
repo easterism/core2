682e0c19d54da35b6a9474fab1ad967059cc4aa6
shabuninil <shabuninil24@gmail.com>
2024-05-23 11:17:49 +0300

    Рефакторинг

--- inc/classes/Fact.php
+++ inc/classes/Fact.php
@@ -43,6 +43,7 @@ class Fact {
 
     /**
      * @param mixed $text
+     * @param bool  $is_public
      * @return void
      */
     public function message(mixed $text, bool $is_public = false): void {
@@ -63,20 +64,30 @@ class Fact {
         $this->topic = 'global';
     }
 
-    public function elementText(string $selector, string $text, bool $is_public = false)
-    {
+
+    /**
+     * Установка текста в указанный селектор на странице
+     * @param string $selector
+     * @param string $text
+     * @param bool   $is_public
+     * @return void
+     */
+    public function elementText(string $selector, string $text, bool $is_public = false): void {
+
         $text = json_encode(['element' => ['selector' => $selector, 'text' => $text]]);
-        $q = !$is_public ? msg_get_queue($this->shm_id) : msg_get_queue($this->shm_public);
-        if (! msg_send($q, self::TEXT, json_encode([$this->topic => $text]), false, true, $msg_err)) {
+        $q    = ! $is_public ? msg_get_queue($this->shm_id) : msg_get_queue($this->shm_public);
+
+        if ( ! msg_send($q, self::TEXT, json_encode([$this->topic => $text]), false, true, $msg_err)) {
             $this->log->error($msg_err);
         };
 
-        if (! in_array($text, $this->messages[$this->topic])) {
+        if ( ! in_array($text, $this->messages[$this->topic])) {
             $this->messages[$this->topic][] = $text;
         }
 
         $this->topic = 'global';
     }
+
 
     /**
      * получаем список всех фактов, которые были добавлены в этом сеансе
