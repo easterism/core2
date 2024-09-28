67de44627bfa6708ced6e42b9e1c72bb83e6599b
easterism <easter.by@gmail.com>
2024-09-28 13:34:51 +0300

    upgrade Navigation

--- inc/classes/Init.php
+++ inc/classes/Init.php
@@ -170,7 +170,6 @@ require_once 'Db.php';
 require_once 'Common.php';
 require_once 'Templater2.php'; //DEPRECATED
 require_once 'Templater3.php';
-require_once 'Login.php';
 require_once 'SSE.php';
 require_once 'Cli.php';
 
@@ -420,13 +419,15 @@ class Init extends Db {
 
             }
             else {
+                require_once 'Login.php';
+
                 $login = new Login();
                 $login->setSystemName($this->getSystemName());
                 $login->setFavicon($this->getSystemFavicon());
                 parse_str($route['query'], $request);
                 $response = $login->dispatch($request); //TODO переделать на API
                 if (!$response) {
-                    //запись сессии не происходит
+                    //Immutable блокирует запись сессии
                     //SessionContainer::getDefaultManager()->getStorage()->markImmutable();
                     $this->setupSkin();
                     $response = $login->getPageLogin();
@@ -445,6 +446,8 @@ class Init extends Db {
                 ($_SERVER['REQUEST_URI'] == $_SERVER['SCRIPT_NAME'] ||
                 trim($_SERVER['REQUEST_URI'], '/') == trim(str_replace("\\", "/", dirname($_SERVER['SCRIPT_NAME'])), '/'))
             ) {
+                require_once 'Navigation.php';
+
                 if (empty($this->auth->init)) { //нет сессии на сервере
                     return $this->getMenuMobile();
                 }
@@ -1147,7 +1150,12 @@ class Init extends Db {
                             }
 
                             if (THEME !== 'default') {
-                                $navigate_items[$module_id] = $this->getModuleNavigation($module['module_id'], $modController);
+                                $navi = new \Core2\Navigation(); //TODO переделать для обработки всех модулей сразу
+                                $navi->setModuleNavigation($module['module_id']);
+                                if ($modController instanceof Navigation) {
+                                    $modController->navigationItems($navi);
+                                }
+                                $navigate_items[$module_id] = $navi->toArray();
                             }
                         }
                         ob_clean();
@@ -1171,6 +1179,7 @@ class Init extends Db {
             }
 
             if ( ! empty($navigate_items)) {
+                $navi = new \Core2\Navigation();
                 foreach ($navigate_items as $module_name => $items) {
                     if ( ! empty($items)) {
                         foreach ($items as $item) {
@@ -1180,7 +1189,7 @@ class Init extends Db {
                                 case 'profile':
                                     if ($tpl_menu->issetBlock('navigate_item_profile')) {
                                         $tpl_menu->navigate_item_profile->assign('[MODULE_NAME]', $module_name);
-                                        $tpl_menu->navigate_item_profile->assign('[HTML]',        $this->renderNavigateItem($item));
+                                        $tpl_menu->navigate_item_profile->assign('[HTML]',        $navi->renderNavigateItem($item));
                                         $tpl_menu->navigate_item_profile->reassign();
                                     }
                                     break;
@@ -1189,7 +1198,7 @@ class Init extends Db {
                                 default:
                                     if ($tpl_menu->issetBlock('navigate_item')) {
                                         $tpl_menu->navigate_item->assign('[MODULE_NAME]', $module_name);
-                                        $tpl_menu->navigate_item->assign('[HTML]',        $this->renderNavigateItem($item));
+                                        $tpl_menu->navigate_item->assign('[HTML]',        $navi->renderNavigateItem($item));
                                         $tpl_menu->navigate_item->reassign();
                                     }
                                     break;
@@ -1249,99 +1258,6 @@ class Init extends Db {
             return $tpl->render();
         }
 
-
-        /**
-         * @param $navigate_item
-         * @return string
-         * @throws Exception
-         */
-        private function renderNavigateItem($navigate_item) {
-
-            if (empty($navigate_item['type'])) {
-                return '';
-            }
-
-            $html = '';
-            switch ($navigate_item['type']) {
-                case 'divider':
-                    $html = file_get_contents(Theme::get("html-navigation-divider"));
-                    break;
-
-                case 'link':
-                    $link = ! empty($navigate_item['link'])
-                        ? $navigate_item['link']
-                        : '#';
-                    $on_click = ! empty($navigate_item['onclick'])
-                        ? $navigate_item['onclick']
-                        : "if (event.button === 0 && ! event.ctrlKey) load('{$link}');";
-
-                    $tpl = new Templater3(Theme::get("html-navigation-link"));
-                    $tpl->assign('[TITLE]',   ! empty($navigate_item['title']) ? $navigate_item['title'] : '');
-                    $tpl->assign('[ICON]',    ! empty($navigate_item['icon']) ? $navigate_item['icon'] : '');
-                    $tpl->assign('[CLASS]',   ! empty($navigate_item['class']) ? $navigate_item['class'] : '');
-                    $tpl->assign('[ID]',      ! empty($navigate_item['id']) ? $navigate_item['id'] : '');
-                    $tpl->assign('[LINK]',    $link);
-                    $tpl->assign('[ONCLICK]', $on_click);
-                    $html = $tpl->render();
-                    break;
-
-                case 'dropdown':
-                    $tpl = new Templater3(Theme::get("html-navigation-dropdown"));
-                    $tpl->assign('[TITLE]', ! empty($navigate_item['title']) ? $navigate_item['title'] : '');
-                    $tpl->assign('[ICON]',  ! empty($navigate_item['icon'])  ? $navigate_item['icon']  : '');
-                    $tpl->assign('[CLASS]', ! empty($navigate_item['class']) ? $navigate_item['class'] : '');
-
-                    if ( ! empty($navigate_item['items'])) {
-                        foreach ($navigate_item['items'] as $list_item) {
-
-                            switch ($list_item['type']) {
-                                case 'link':
-                                    $link = ! empty($list_item['link'])
-                                        ? $list_item['link']
-                                        : '#';
-                                    $on_click = ! empty($list_item['onclick'])
-                                        ? $list_item['onclick']
-                                        : "if (event.button === 0 && ! event.ctrlKey) load('{$link}');";
-
-                                    $tpl->item->link->assign('[TITLE]',   ! empty($list_item['title']) ? $list_item['title'] : '');
-                                    $tpl->item->link->assign('[ICON]',    ! empty($list_item['icon']) ? $list_item['icon'] : '');
-                                    $tpl->item->link->assign('[CLASS]',   ! empty($list_item['class']) ? $list_item['class'] : '');
-                                    $tpl->item->link->assign('[ID]',      ! empty($list_item['id']) ? $list_item['id'] : '');
-                                    $tpl->item->link->assign('[LINK]',    $link);
-                                    $tpl->item->link->assign('[ONCLICK]', $on_click);
-                                    break;
-
-                                case 'file':
-                                    $on_change = ! empty($list_item['onchange'])
-                                        ? $list_item['onchange']
-                                        : "";
-
-                                    $tpl->item->file->assign('[TITLE]',    ! empty($list_item['title']) ? $list_item['title'] : '');
-                                    $tpl->item->file->assign('[ICON]',     ! empty($list_item['icon']) ? $list_item['icon'] : '');
-                                    $tpl->item->file->assign('[CLASS]',    ! empty($list_item['class']) ? $list_item['class'] : '');
-                                    $tpl->item->file->assign('[ID]',       ! empty($list_item['id']) ? $list_item['id'] : '');
-                                    $tpl->item->file->assign('[ONCHANGE]', $on_change);
-                                    break;
-
-                                case 'divider':
-                                    $tpl->item->touchBlock('divider');
-                                    break;
-
-                                case 'header':
-                                    $tpl->item->header->assign('[TITLE]', ! empty($list_item['title']) ? $list_item['title'] : '');
-                                    break;
-                            }
-
-                            $tpl->item->reassign();
-                        }
-                    }
-
-                    $html = $tpl->render();
-                    break;
-            }
-
-            return $html;
-        }
 
 
         /**
@@ -1422,134 +1338,6 @@ class Init extends Db {
         }
 
 
-        /**
-         * @param $name
-         * @param $mod_controller
-         * @return array
-         * @throws Zend_Config_Exception
-         */
-        private function getModuleNavigation($name, $mod_controller): array {
-
-            require_once 'Navigation.php';
-
-            $config_module = $this->getModuleConfig($name);
-            $navigation    = new Core2\Navigation();
-
-            if ( ! empty($config_module) &&
-                 ! empty($config_module->system) &&
-                 ! empty($config_module->system->nav)
-            ) {
-                $navigations = $config_module->system->nav->toArray();
-
-                if ( ! empty($navigations)) {
-                    foreach ($navigations as $key => $nav) {
-                        if ( ! empty($nav['type'])) {
-                            $nav['position'] = $nav['position'] ?? '';
-
-                            switch ($nav['type']) {
-                                case 'link':
-                                    $nav['title'] = $nav['title'] ?? '';
-                                    $nav['link']  = $nav['link'] ?? '#';
-
-                                    $nav_link = $navigation->addLink($nav['title'], $nav['link'], $nav['position']);
-
-                                    if ( ! empty($nav['icon'])) {
-                                        $nav_link->setIcon($nav['icon']);
-                                    }
-                                    if ( ! empty($nav['id'])) {
-                                        $nav_link->setId($nav['id']);
-                                    }
-                                    if ( ! empty($nav['class'])) {
-                                        $nav_link->setClass($nav['class']);
-                                    }
-                                    if ( ! empty($nav['onclick'])) {
-                                        $nav_link->setOnClick($nav['onclick']);
-                                    }
-                                    break;
-
-                                case 'divider':
-                                    $navigation->addDivider($nav['position']);
-                                    break;
-
-                                case 'dropdown':
-                                    $nav['title'] = $nav['title'] ?? '';
-                                    $nav['items'] = $nav['items'] ?? [];
-
-                                    $nav_list = $navigation->addDropdown($nav['title'], $nav['position']);
-
-                                    if ( ! empty($nav['icon'])) {
-                                        $nav_list->setIcon($nav['icon']);
-                                    }
-                                    if ( ! empty($nav['class'])) {
-                                        $nav_list->setClass($nav['class']);
-                                    }
-
-                                    if ( ! empty($nav['items'])) {
-                                        foreach ($nav['items'] as $item) {
-
-                                            switch ($item['type']) {
-                                                case 'link':
-                                                    $item['title'] = $item['title'] ?? '';
-                                                    $item['link']  = $item['link'] ?? '#';
-
-                                                    $item_link = $nav_list->addLink($item['title'], $item['link']);
-
-                                                    if ( ! empty($item['id'])) {
-                                                        $item_link->setId($item['id']);
-                                                    }
-                                                    if ( ! empty($item['class'])) {
-                                                        $item_link->setClass($item['class']);
-                                                    }
-                                                    if ( ! empty($item['icon'])) {
-                                                        $item_link->setIcon($item['icon']);
-                                                    }
-                                                    if ( ! empty($item['onclick'])) {
-                                                        $item_link->setOnClick($item['onclick']);
-                                                    }
-                                                    break;
-
-                                                case 'header':
-                                                    $item['title'] = $item['title'] ?? '';
-                                                    $nav_list->addHeader($item['title']);
-                                                    break;
-
-                                                case 'divider':
-                                                    $nav_list->addDivider();
-                                                    break;
-
-                                                case 'file':
-                                                    $item['title'] = $item['title'] ?? '';
-                                                    $item_file = $nav_list->addFile($item['title']);
-
-                                                    if ( ! empty($item['id'])) {
-                                                        $item_file->setId($item['id']);
-                                                    }
-                                                    if ( ! empty($item['class'])) {
-                                                        $item_file->setClass($item['class']);
-                                                    }
-                                                    if ( ! empty($item['icon'])) {
-                                                        $item_file->setIcon($item['icon']);
-                                                    }
-                                                    if ( ! empty($item['onchange'])) {
-                                                        $item_file->setOnChange($item['onchange']);
-                                                    }
-                                                    break;
-                                            }
-                                        }
-                                    }
-                                    break;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if ($mod_controller instanceof Navigation) {
-                $mod_controller->navigationItems($navigation);
-            }
-
-            return $navigation->toArray();
-        }
 
 
         /**
--- inc/classes/Navigation.php
+++ inc/classes/Navigation.php
@@ -1,14 +1,21 @@
 <?php
 namespace Core2;
+
+require_once 'Db.php';
+require_once 'Navigation/Divider.php';
+require_once 'Navigation/Link.php';
+require_once 'Navigation/Dropdown.php';
+
 use Core2\Navigation\Divider;
 use Core2\Navigation\Dropdown;
 use Core2\Navigation\Link;
+use \Templater3;
 
 
 /**
  *
  */
-class Navigation {
+class Navigation extends Db {
 
     /**
      * @var array
@@ -25,7 +32,7 @@ class Navigation {
      */
     public function addLink(string $title, string $link, string $position = ''): Link {
 
-        require_once 'Navigation/Link.php';
+
         $nav_link = new Link();
         $nav_link->setTitle($title);
         $nav_link->setLink($link);
@@ -47,7 +54,7 @@ class Navigation {
      */
     public function addDivider(string $position = ''): Divider {
 
-        require_once 'Navigation/Divider.php';
+
         $nav_divider = new Divider();
 
         if ($position) {
@@ -68,7 +75,7 @@ class Navigation {
      */
     public function addDropdown(string $title, string $position = ''): Dropdown {
 
-        require_once 'Navigation/Dropdown.php';
+
         $nav_dropdown = new Dropdown();
         $nav_dropdown->setTitle($title);
 
@@ -98,4 +105,221 @@ class Navigation {
 
         return $data;
     }
+
+
+    /**
+     * @param $name
+     * @param $mod_controller
+     * @return array
+     * @throws Zend_Config_Exception
+     */
+    public function setModuleNavigation($name): void {
+
+        $config_module = $this->getModuleConfig($name);
+
+        if ( ! empty($config_module) &&
+            ! empty($config_module->system) &&
+            ! empty($config_module->system->nav)
+        ) {
+            $navigations = $config_module->system->nav->toArray();
+
+            if ( ! empty($navigations)) {
+                foreach ($navigations as $key => $nav) {
+                    if ( ! empty($nav['type'])) {
+                        $nav['position'] = $nav['position'] ?? '';
+
+                        switch ($nav['type']) {
+                            case 'link':
+                                $nav['title'] = $nav['title'] ?? '';
+                                $nav['link']  = $nav['link'] ?? '#';
+
+                                $nav_link = $this->addLink($nav['title'], $nav['link'], $nav['position']);
+
+                                if ( ! empty($nav['icon'])) {
+                                    $nav_link->setIcon($nav['icon']);
+                                }
+                                if ( ! empty($nav['id'])) {
+                                    $nav_link->setId($nav['id']);
+                                }
+                                if ( ! empty($nav['class'])) {
+                                    $nav_link->setClass($nav['class']);
+                                }
+                                if ( ! empty($nav['onclick'])) {
+                                    $nav_link->setOnClick($nav['onclick']);
+                                }
+                                break;
+
+                            case 'divider':
+                                $this->addDivider($nav['position']);
+                                break;
+
+                            case 'dropdown':
+                                $nav['title'] = $nav['title'] ?? '';
+                                $nav['items'] = $nav['items'] ?? [];
+
+                                $nav_list = $this->addDropdown($nav['title'], $nav['position']);
+
+                                if ( ! empty($nav['icon'])) {
+                                    $nav_list->setIcon($nav['icon']);
+                                }
+                                if ( ! empty($nav['class'])) {
+                                    $nav_list->setClass($nav['class']);
+                                }
+
+                                if ( ! empty($nav['items'])) {
+                                    foreach ($nav['items'] as $item) {
+
+                                        switch ($item['type']) {
+                                            case 'link':
+                                                $item['title'] = $item['title'] ?? '';
+                                                $item['link']  = $item['link'] ?? '#';
+
+                                                $item_link = $nav_list->addLink($item['title'], $item['link']);
+
+                                                if ( ! empty($item['id'])) {
+                                                    $item_link->setId($item['id']);
+                                                }
+                                                if ( ! empty($item['class'])) {
+                                                    $item_link->setClass($item['class']);
+                                                }
+                                                if ( ! empty($item['icon'])) {
+                                                    $item_link->setIcon($item['icon']);
+                                                }
+                                                if ( ! empty($item['onclick'])) {
+                                                    $item_link->setOnClick($item['onclick']);
+                                                }
+                                                break;
+
+                                            case 'header':
+                                                $item['title'] = $item['title'] ?? '';
+                                                $nav_list->addHeader($item['title']);
+                                                break;
+
+                                            case 'divider':
+                                                $nav_list->addDivider();
+                                                break;
+
+                                            case 'file':
+                                                $item['title'] = $item['title'] ?? '';
+                                                $item_file = $nav_list->addFile($item['title']);
+
+                                                if ( ! empty($item['id'])) {
+                                                    $item_file->setId($item['id']);
+                                                }
+                                                if ( ! empty($item['class'])) {
+                                                    $item_file->setClass($item['class']);
+                                                }
+                                                if ( ! empty($item['icon'])) {
+                                                    $item_file->setIcon($item['icon']);
+                                                }
+                                                if ( ! empty($item['onchange'])) {
+                                                    $item_file->setOnChange($item['onchange']);
+                                                }
+                                                break;
+                                        }
+                                    }
+                                }
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+
+    /**
+     * @param $navigate_item
+     * @return string
+     * @throws Exception
+     */
+    public function renderNavigateItem($navigate_item) {
+
+        if (empty($navigate_item['type'])) {
+            return '';
+        }
+
+        $html = '';
+        switch ($navigate_item['type']) {
+            case 'divider':
+                $html = file_get_contents(Theme::get("html-navigation-divider"));
+                break;
+
+            case 'link':
+                $link = ! empty($navigate_item['link'])
+                    ? $navigate_item['link']
+                    : '#';
+                $on_click = ! empty($navigate_item['onclick'])
+                    ? $navigate_item['onclick']
+                    : "if (event.button === 0 && ! event.ctrlKey) load('{$link}');";
+
+                $tpl = new Templater3(Theme::get("html-navigation-link"));
+                $tpl->assign('[TITLE]',   ! empty($navigate_item['title']) ? $navigate_item['title'] : '');
+                $tpl->assign('[ICON]',    ! empty($navigate_item['icon']) ? $navigate_item['icon'] : '');
+                $tpl->assign('[CLASS]',   ! empty($navigate_item['class']) ? $navigate_item['class'] : '');
+                $tpl->assign('[ID]',      ! empty($navigate_item['id']) ? $navigate_item['id'] : '');
+                $tpl->assign('[LINK]',    $link);
+                $tpl->assign('[ONCLICK]', $on_click);
+                $html = $tpl->render();
+                break;
+
+            case 'dropdown':
+                $tpl = new Templater3(Theme::get("html-navigation-dropdown"));
+                $tpl->assign('[TITLE]', ! empty($navigate_item['title']) ? $navigate_item['title'] : '');
+                $tpl->assign('[ICON]',  ! empty($navigate_item['icon'])  ? $navigate_item['icon']  : '');
+                $tpl->assign('[CLASS]', ! empty($navigate_item['class']) ? $navigate_item['class'] : '');
+
+                if ( ! empty($navigate_item['items'])) {
+                    foreach ($navigate_item['items'] as $list_item) {
+
+                        switch ($list_item['type']) {
+                            case 'link':
+                                $link = ! empty($list_item['link'])
+                                    ? $list_item['link']
+                                    : '#';
+                                $on_click = ! empty($list_item['onclick'])
+                                    ? $list_item['onclick']
+                                    : "if (event.button === 0 && ! event.ctrlKey) load('{$link}');";
+
+                                $tpl->item->link->assign('[TITLE]',   ! empty($list_item['title']) ? $list_item['title'] : '');
+                                $tpl->item->link->assign('[ICON]',    ! empty($list_item['icon']) ? $list_item['icon'] : '');
+                                $tpl->item->link->assign('[CLASS]',   ! empty($list_item['class']) ? $list_item['class'] : '');
+                                $tpl->item->link->assign('[ID]',      ! empty($list_item['id']) ? $list_item['id'] : '');
+                                $tpl->item->link->assign('[LINK]',    $link);
+                                $tpl->item->link->assign('[ONCLICK]', $on_click);
+                                break;
+
+                            case 'file':
+                                $on_change = ! empty($list_item['onchange'])
+                                    ? $list_item['onchange']
+                                    : "";
+
+                                $tpl->item->file->assign('[TITLE]',    ! empty($list_item['title']) ? $list_item['title'] : '');
+                                $tpl->item->file->assign('[ICON]',     ! empty($list_item['icon']) ? $list_item['icon'] : '');
+                                $tpl->item->file->assign('[CLASS]',    ! empty($list_item['class']) ? $list_item['class'] : '');
+                                $tpl->item->file->assign('[ID]',       ! empty($list_item['id']) ? $list_item['id'] : '');
+                                $tpl->item->file->assign('[ONCHANGE]', $on_change);
+                                break;
+
+                            case 'divider':
+                                $tpl->item->touchBlock('divider');
+                                break;
+
+                            case 'header':
+                                $tpl->item->header->assign('[TITLE]', ! empty($list_item['title']) ? $list_item['title'] : '');
+                                break;
+                        }
+
+                        $tpl->item->reassign();
+                    }
+                }
+
+                $html = $tpl->render();
+                break;
+        }
+
+        return $html;
+    }
+
 }
