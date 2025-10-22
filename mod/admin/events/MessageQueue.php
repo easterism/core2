ea20de663cdc1dacc49c411cf8b6752851255906
easterism <easter.by@gmail.com>
2025-10-22 16:50:42 +0300

    SSE Fact

--- mod/admin/events/MessageQueue.php
+++ mod/admin/events/MessageQueue.php
@@ -1,6 +1,7 @@
 <?php
-
 namespace Core2;
+
+require_once 'core2/inc/Interfaces/Event.php';
 
 /**
  * Class UnreadMessages
@@ -10,16 +11,27 @@ namespace Core2;
 class MessageQueue extends \Common implements Event {
 
     private $queue;
+    private $_q;
+
     private $_messages = [];
 
     const TEXT = 1;
+
+
+    public function __construct(\SysvMessageQueue|false $queue)
+    {
+        if ($queue) $this->queue = $queue;
+        $this->_q = (msg_get_queue(ftok(__FILE__, 't') + crc32($_SERVER['SERVER_NAME'])));
+        parent::__construct();
+    }
 
     /**
      * @return array|string
      */
     public function dispatch() {
-
-        return $this->_messages;
+        $out = $this->_messages;
+        $this->_messages = [];
+        return $out;
     }
 
 
@@ -28,31 +40,29 @@ class MessageQueue extends \Common implements Event {
      */
     public function check(): bool {
 
-        if (!$this->queue) return false;
+        if (!$this->_q) {
+            //пробуем переподключиться к системной очереди
+            $this->_q = msg_get_queue(ftok(__FILE__, 't') + crc32($_SERVER['SERVER_NAME']));
+        }
+        if ($this->_q) {
+            msg_receive($this->_q, self::TEXT, $message_type, 10000, $message, false, MSG_IPC_NOWAIT | MSG_NOERROR, $error);
+            if (trim($message)) {
+                $this->_messages[] = $message;
+            }
+        }
 
+        if ($this->queue) {
+            //юзерная очередь
+            $message = '';
 //        $queue_status = msg_stat_queue($this->queue); //статистика очереди
-        $hasMessages = true;
-        $from_queue = [];
-        while ($hasMessages) {
-            //забираем сообщения с типом 1
-            //TODO придумать разные типы сообщений
             msg_receive($this->queue, self::TEXT, $message_type, 10000, $message, false, MSG_IPC_NOWAIT | MSG_NOERROR, $error);
-            if ($message) {
-                $message = json_decode($message, true);
-                if (!in_array($message, $from_queue)) $from_queue[key($message)][] = current($message);
-            } else $hasMessages = false;
-//            usleep(1000);
-        }
-        if ($from_queue) {
-            $this->_messages = $from_queue;
-            return true;
+            if (trim($message)) {
+                $this->_messages[] = $message;
+            }
         }
 
-        return false;
+
+        return count($this->_messages) ? true : false;
     }
 
-    public function setQueue(\SysvMessageQueue|false $q)
-    {
-        if ($q) $this->queue = $q;
-    }
 }
